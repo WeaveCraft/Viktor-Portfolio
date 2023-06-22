8f1ac947b45d1cf0e48b13c0da4a14892876767d
Viktor Hurtig <Viktor.hurtig@outlook.com>
2023-06-22 17:23:29 +0200

    feat(projects): Added petpal page.

--- pages/projects/berrasbio.js
+++ pages/projects/berrasbio.js
@@ -1,60 +1,60 @@
 import {
-    Container,
-    Badge,
-    Link,
-    List,
-    ListItem,
-    AspectRatio
-  } from '@chakra-ui/react'
-  import { ExternalLinkIcon } from '@chakra-ui/icons'
-  import { Title, ProjectImage, Meta } from '../../components/projectInfo'
-  import P from '../../components/styles/paragraph'
-  import Layout from '../../components/layouts/article'
-  
-  const Project = () => (
-    <Layout title="BerrasBio">
-      <Container>
-        <Title>
-          BerrasBio <Badge>2022-</Badge>
-        </Title>
-        <P>
-          A meetup application for dog and dog owners to chat, like and share interests.
-        </P>
-        <List ml={4} my={4}>
-          <ListItem>
-            <Meta>Repository</Meta>
-            <Link href="https://github.com/WeaveCraft/BerrasBio">
-              Link <ExternalLinkIcon mx="2px" />
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Meta>Platform</Meta>
-            <span>Windows/macOS/Linux/iOS/Android</span>
-          </ListItem>
-          <ListItem>
-            <Meta>Stack</Meta>
-            <span>.Net C#, Razor Pages, SQL Server</span>
-          </ListItem>
-          <ListItem>
-            <Meta>Agile Tools</Meta>
-               
-          </ListItem>
-        </List>
-  
-        <ProjectImage src="/images/projects/berraCinemaApp.png" alt="Inkdrop" />
-        <ProjectImage src="/images/projects/HamsterWarApp.png" alt="Inkdrop" />
-        <AspectRatio maxW="640px" ratio={1.7} my={4}>
-          <iframe
-            src="https://www.youtube.com/embed/XjkHDbAt5kc?controls"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </AspectRatio>
-      </Container>
-    </Layout>
-  )
-  
-  export default Project
-  export { getServerSideProps } from '../../components/chakra'
+  Container,
+  Badge,
+  Link,
+  List,
+  ListItem,
+  Box,
+  Heading
+} from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, ProjectImage, Meta } from '../../components/projectInfo'
+import P from '../../components/styles/paragraph'
+import Paragraph from '../../components/styles/paragraph'
+import Layout from '../../components/layouts/article'
+import { Divider } from '@chakra-ui/react'
+
+const Project = () => (
+  <Layout title="BerrasBio">
+    <Container>
+      <Title>
+        BerrasBio <Badge>2022-</Badge>
+      </Title>
+      <P>
+        A meetup application for dog and dog owners to chat, like and share interests.
+      </P>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Repository</Meta>
+          <Link href="https://github.com/WeaveCraft/BerrasBio">
+            Link <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Platform</Meta>
+          <span>Windows/macOS/Linux/iOS/Android</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span>.NET/C#/Razor Pages/SQL Server/T-SQL</span>
+        </ListItem>
+      </List>
+
+      <Divider />
+
+      <Heading as="h3" fontSize={20} mb={4} variant="section-title">
+        Problem
+      </Heading>
+      <Box borderRadius="lg" bg="#363636" p={4} mb={8} color="white">
+        <Paragraph>
+          Lately more dog owners has been appearing, therefore I have seen the need of someway to broaden the way to network with other dog owners to share their struggles and join together to create a network for sharing knowledge and help dogs aswell as their owners to become friends with others.
+        </Paragraph>
+      </Box>
+
+      <ProjectImage src="/images/projects/PetPal-Structure.png" alt="Inkdrop" />
+    </Container>
+  </Layout>
+)
+
+export default Project
+export { getServerSideProps } from '../../components/chakra'
--- pages/projects/petpal.js
+++ pages/projects/petpal.js
@@ -1,60 +1,114 @@
 import {
-    Container,
-    Badge,
-    Link,
-    List,
-    ListItem,
-    AspectRatio
-  } from '@chakra-ui/react'
-  import { ExternalLinkIcon } from '@chakra-ui/icons'
-  import { Title, ProjectImage, Meta } from '../../components/projectInfo'
-  import P from '../../components/styles/paragraph'
-  import Layout from '../../components/layouts/article'
-  
-  const Project = () => (
-    <Layout title="PetPal">
-      <Container>
-        <Title>
-          PetPal <Badge>2023-</Badge>
-        </Title>
-        <P>
-          A meetup application for dog and dog owners to chat, like and share interests.
-        </P>
-        <List ml={4} my={4}>
-          <ListItem>
-            <Meta>Repository</Meta>
-            <Link href="https://github.com/WeaveCraft/PetPal">
-              Link <ExternalLinkIcon mx="2px" />
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Meta>Platform</Meta>
-            <span>Windows/macOS/Linux/iOS/Android</span>
-          </ListItem>
-          <ListItem>
-            <Meta>Stack</Meta>
-            <span>.Net, SQL Server, Angular 2</span>
-          </ListItem>
-          <ListItem>
-            <Meta>Agile Tools</Meta>
-              Trello used for g 
-          </ListItem>
-        </List>
-  
-        <ProjectImage src="/images/projects/berraCinemaApp.png" alt="Inkdrop" />
-        <ProjectImage src="/images/projects/HamsterWarApp.png" alt="Inkdrop" />
-        <AspectRatio maxW="640px" ratio={1.7} my={4}>
-          <iframe
-            src="https://www.youtube.com/embed/XjkHDbAt5kc?controls"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </AspectRatio>
-      </Container>
-    </Layout>
-  )
-  
-  export default Project
-  export { getServerSideProps } from '../../components/chakra'
+  Container,
+  Badge,
+  Link,
+  List,
+  ListItem,
+  Box,
+  Heading
+} from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, ProjectImage, Meta } from '../../components/projectInfo'
+import P from '../../components/styles/paragraph'
+import Paragraph from '../../components/styles/paragraph'
+import Layout from '../../components/layouts/article'
+import { Divider } from '@chakra-ui/react'
+import { BioSection, BioYear } from '../../components/bio'
+import Section from '../../components/styles/section'
+
+const Project = () => (
+  <Layout title="PetPal">
+    <Container>
+      <Title>
+        PetPal <Badge>2023-</Badge>
+      </Title>
+      <P>
+        A meetup application for dog and dog owners to chat, like and share interests.
+      </P>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Repository</Meta>
+          <Link href="https://github.com/WeaveCraft/PetPal">
+            Link <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Platform</Meta>
+          <span>Windows/macOS/Linux</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span>.Net, SQL Server, Angular 2</span>
+        </ListItem>
+      </List>
+      <Divider />
+
+      <Heading as="h3" fontSize={20} mb={4} variant="section-title">
+        Problem
+      </Heading>
+      <Box borderRadius="lg" bg="#363636" p={4} mb={8} color="white">
+        <Paragraph>
+          Recently, there has been an increase in the number of dog owners, highlighting the growing need for a platform that facilitates networking and collaboration among them.
+          Recognizing this need, I have realized the importance of creating a space where dog owners can connect, share their experiences, and support one another.
+          By fostering a community-driven environment, I aim to promote knowledge sharing, friendship formation, and ultimately improve the well-being of both dogs and their owners.
+        </Paragraph>
+      </Box>
+
+      <Section delay={0.2}>
+        <Heading as="h3" fontSize={20} mb={4} variant="section-title">
+          Solution
+        </Heading>
+        <BioSection>
+          <BioYear>Backend</BioYear>
+          .NET Core to develop the backend application.
+        </BioSection>
+        <BioSection>
+          <BioYear>Backend</BioYear>
+          SQL Server database to store data related to dog owners.
+        </BioSection>
+        <BioSection>
+          <BioYear>Backend</BioYear>
+          RESTful APIs using ASP.NET Web API to handle CRUD.
+        </BioSection>
+        <BioSection>
+          <BioYear>Backend</BioYear>
+          Entity Framework as an ORM tool.
+        </BioSection>
+        <Divider my={4} />
+        <BioSection>
+          <BioYear>Frontend</BioYear>
+          Angular 2 to develop the client side application.
+        </BioSection>
+        <BioSection>
+          <BioYear>Frontend</BioYear>
+          HTTP to communicate with the .NET APIs controllers.
+        </BioSection>
+        <BioSection>
+          <BioYear>Frontend</BioYear>
+          Angular routing to navigate between different views.
+        </BioSection>
+        <BioSection>
+          <BioYear>Frontend</BioYear>
+          Angular features to handle data binding.
+        </BioSection>
+      </Section>
+
+
+      <Heading as="h3" fontSize={20} mb={4} variant="section-title">
+        Result
+      </Heading>
+      <Box borderRadius="lg" bg="#363636" p={4} mb={8} color="white">
+        <Paragraph>
+        The combined result of the backend and frontend implementation made it to be a functional meeting web app for dog owners.
+         This app allows dog owners to connect, share information, and organize meetings or events related to their dogs. 
+         Users would be able to create profiles, search for other dog owners, chat and like other users, and interact with the community of dog owners through the app's user interface.
+        </Paragraph>
+      </Box>
+
+      <ProjectImage src="/images/projects/PetPal-Structure.png" alt="Inkdrop" />
+    </Container>
+  </Layout>
+)
+
+export default Project
+export { getServerSideProps } from '../../components/chakra'

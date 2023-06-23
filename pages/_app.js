a1a7a0fea64e09edba266ef1a1adba43c385b7e3
Viktor Hurtig <Viktor.hurtig@outlook.com>
2023-06-23 22:52:51 +0200

    fix(libs): fixed libs folder  to -> lib now.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Fonts from '../components/fonts'
 import Layout from '../components/layouts/main'
-import theme from '../libs/theme'
+import theme from '../lib/theme'
 import { AnimatePresence } from 'framer-motion'
 
 const Website = ({ Component, pageProps, router }) => {
